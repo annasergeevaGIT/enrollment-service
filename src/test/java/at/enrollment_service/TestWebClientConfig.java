c4d52d96bc2a893c354a22e5aa097b67ea5d3140
Sergeeva Anna <sirgeeva1@gmail.com>
2025-11-18 22:56:53 +0100

    fix: virtual threads

---
+++ src/test/java/at/enrollment_service/TestWebClientConfig.java
@@ -0,0 +1,22 @@
+package at.enrollment_service;
+
+import at.enrollment_service.config.EnrollmentServiceProps;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+
+@TestConfiguration
+public class TestWebClientConfig {
+
+    private final EnrollmentServiceProps props;
+
+    public TestWebClientConfig(EnrollmentServiceProps props) {
+        this.props = props;
+    }
+
+    @Bean
+    // CHANGE: Method signature changed to return RestClient
+    public RestClient restClient(RestClient.Builder builder) {
+        return builder.baseUrl(props.getCourseServiceUrl()).build();
+    }
+}
