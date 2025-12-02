aeb128d8b20664134598c69cdb24ecdd6ec2a89a
Sergeeva Anna <sirgeeva1@gmail.com>
2025-12-02 22:12:28 +0100

    fix: virtual threads

--- src/main/java/at/enrollment_service/mapper/CourseClient.java
+++ src/main/java/at/enrollment_service/mapper/CourseClient.java
@@ -3,11 +3,7 @@ package at.enrollment_service.mapper;
 import at.enrollment_service.config.EnrollmentServiceProps;
 import at.enrollment_service.dto.GetCourseInfoRequest;
 import at.enrollment_service.dto.GetCourseInfoResponse;
-import at.enrollment_service.exception.EnrollmentServiceException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
